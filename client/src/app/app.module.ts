c9a5c7e8387df57505932aad8a2eafde435582e6
Kovacs Arthur Kraid <kovacs.arthur.kraid@gmail.com>
2018-03-12 19:55:25 +0200

    First Commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,145 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+import { LottieAnimationViewModule } from 'ng-lottie';
+import { PixiModule } from 'angular2pixi/dist/es/index';
+import 'pixi-svg/dist/pixi-svg';
+import 'pixi-sound/dist/pixi-sound.es';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { CustomRouterStateSerializer } from './app.utils';
+
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+import { environment } from 'environments/environment';
+import { ROUTES } from './app.routes';
+// App is our top level component
+import { AppComponent } from './app.component';
+import { APP_RESOLVER_PROVIDERS } from './app.resolver';
+import { AppState, InternalStateType } from './app.service';
+import { HomeComponent } from './home';
+import { AboutComponent } from './about';
+import { NoContentComponent } from './no-content';
+import { XLargeDirective } from './home/x-large';
+import { DevModuleModule } from './+dev-module';
+
+import '../styles/styles.scss';
+import '../styles/headings.css';
+
+import { RotatingDialComponent } from './rotating-dial/rotating-dial.component';
+import { DialSceneComponent } from './rotating-dial/dial-scene.component';
+import { LoadingAnimationComponent } from './logo-animation/logo-animation.component';
+
+// Application wide providers
+const APP_PROVIDERS = [
+  ...APP_RESOLVER_PROVIDERS,
+  AppState
+];
+
+interface StoreType {
+  state: InternalStateType;
+  restoreInputValues: () => void;
+  disposeOldHosts: () => void;
+}
+
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
+@NgModule({
+  bootstrap: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    LoadingAnimationComponent,
+    DialSceneComponent,
+    RotatingDialComponent,
+    AboutComponent,
+    HomeComponent,
+    NoContentComponent,
+    XLargeDirective
+  ],
+  /**
+   * Import Angular's modules.
+   */
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpClientModule,
+    LottieAnimationViewModule.forRoot(),
+    PixiModule,
+    RouterModule.forRoot(ROUTES, {
+      useHash: Boolean(history.pushState) === false,
+      preloadingStrategy: PreloadAllModules
+    }),
+
+    /**
+     * This section will import the `DevModuleModule` only in certain build types.
+     * When the module is not imported it will get tree shaked.
+     * This is a simple example, a big app should probably implement some logic
+     */
+    ...environment.showDevModule ? [ DevModuleModule ] : [],
+
+    // StoreModule.forRoot(reducers, { metaReducers }),
+
+    /**
+     * @ngrx/router-store keeps router state up-to-date in the store.
+     */
+    /*StoreRouterConnectingModule.forRoot({
+      /!*
+        They stateKey defines the name of the state used by the router-store reducer.
+        This matches the key defined in the map of reducers
+      *!/
+      stateKey: 'router',
+    }),*/
+
+    /**
+     * Store devtools instrument the store retaining past versions of state
+     * and recalculating new states. This enables powerful time-travel
+     * debugging.
+     *
+     * To use the debugger, install the Redux Devtools extension for either
+     * Chrome or Firefox
+     *
+     * See: https://github.com/zalmoxisus/redux-devtools-extension
+     */
+    /*StoreDevtoolsModule.instrument({
+      name: 'Arthur App Store DevTools',
+      logOnly: environment.production,
+    }),*/
+
+    /**
+     * EffectsModule.forRoot() is imported once in the root module and
+     * sets up the effects class to be initialized immediately when the
+     * application starts.
+     *
+     * See: https://github.com/ngrx/platform/blob/master/docs/effects/api.md#forroot
+     */
+    // EffectsModule.forRoot([]),
+  ],
+  /**
+   * Expose our Services and Providers into Angular's dependency injection.
+   */
+  providers: [
+    environment.ENV_PROVIDERS,
+    APP_PROVIDERS,
+    /**
+     * The `RouterStateSnapshot` provided by the `Router` is a large complex structure.
+     * A custom RouterStateSerializer is used to parse the `RouterStateSnapshot` provided
+     * by `@ngrx/router-store` to include only the desired pieces of the snapshot.
+     */
+    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
+  ]
+})
+export class AppModule {}
